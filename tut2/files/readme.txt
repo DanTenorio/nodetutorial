bc52442fe6a274712f68441f8dc15d9c8871b3dc
daniel tenorio <juantenorio16@augustana.edu>
2022-12-01 11:38:30 -0600

    Partial completion of tut2

---
+++ tut2/files/readme.txt
@@ -0,0 +1,3 @@
+node js for documentation
+select correct version
+select correct module e.g. file system and ztrl+f to find documentation
---
+++ tut2/index.js
@@ -0,0 +1,73 @@
+const fsPromises = require('fs').promises;
+// const fs = require('fs');
+const path = require('path');
+
+const fileOps = async() => {
+    try{
+        const data = await fsPromises.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf8');
+        console.log(data);
+        await fsPromises.unlink(path.join(__dirname, 'files', 'starter.txt'), data)
+        await fsPromises.writeFile(path.join(__dirname, 'files', 'promiseWrite.txt'), data);
+        await fsPromises.appendFile(path.join(__dirname, 'files', 'promiseWrite.txt'), '\nNice to meet you!');
+        await fsPromises.rename(path.join(__dirname, 'files', 'promiseWrite.txt'), path.join(__dirname, 'files', 'promiseComplete.txt'));
+        const newData = await fsPromises.readFile(path.join(__dirname, 'files', 'promiseComplete.txt'), 'utf8');
+        console.log(newData);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+fileOps();
+
+// fs.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf8', (err, data) => {
+//     if(err) throw err;
+//     console.log(data);
+//     console.log("----------------------------------------------------");
+// })
+
+// fs.readFile("./files/starter.txt", (err, data) => {
+//     if(err) throw err;
+//     console.log(data.toString());
+//     console.log("----------------------------------------------------");
+// })
+
+// fs.readFile("./files/starter.txt", 'utf8', (err, data) => {
+//     if(err) throw err;
+//     console.log(data);
+//     console.log("----------------------------------------------------");
+// })
+
+// //Shows that node is async
+// console.log('Hello...')
+
+// fs.writeFile(path.join(__dirname, 'files', 'reply.txt'), "Nice to meet you Danny", (err) => {
+//     if(err) throw err;
+//     console.log("Write complete");
+//     console.log("----------------------------------------------------");
+
+//     //Since node is async this prevents write happening after append
+//     fs.appendFile(path.join(__dirname, 'files', 'reply.txt'), "\nHow are you?", (err) => {
+//         if(err) throw err;
+//         console.log("Append complete");
+//         console.log("----------------------------------------------------");
+
+//         fs.rename(path.join(__dirname, 'files', 'reply.txt'), path.join(__dirname, 'files', 'replyEdit.txt'), err => {
+//             if(err) throw err;
+//             console.log("Rename complete");
+//             console.log("----------------------------------------------------");
+//         })
+//     })
+// })
+
+
+// fs.appendFile(path.join(__dirname, 'files', 'reply2.txt'), "How are you?", (err) => {
+//     if(err) throw err;
+//     console.log("Append complete");
+//     console.log("----------------------------------------------------");
+// })
+
+//Exit on uncaught errors
+process.on('uncaughtException', err => {
+    console.log(`There was an uncaught error: ${err}`);
+    process.exit(1);
+})
